21317ec42ef0c6107b476267e41aa35bef4f2cf0
Khanh Hung <hung@swagsoft.com>
2020-11-12 21:42:23 +0700

    commit

--- src/components/Layout/Consumer/FooterConsumer.js
+++ src/components/Layout/Consumer/FooterConsumer.js
@@ -1,16 +1,27 @@
 import React from "react";
 import "./FooterConsumer.css";
+import { Row, Col } from "reactstrap";
 
 function FooterConsumer() {
   return (
     <div className="FooterCosumer">
       <div className="FooterCosumer__content screen__Wep">
-        <div className="FooterCosumer__content__left">
-          <span>Liên Hệ</span>
-        </div>
-        <div className="FooterCosumer__content__Center">
-          <span>Chính Sách</span>
-        </div>
+        <Row className="FooterCosumer__content__Content">
+          <Col
+            lg={4}
+            md={4}
+            sm={12}
+            className="FooterCosumer__content__Content__Left"
+          >
+            <span>Liên Hệ</span>
+          </Col>
+          <Col lg={4} md={4} sm={12}>
+            Chính Sách
+          </Col>
+          <Col lg={4} md={4} sm={12}>
+            Bảo Trì SOFA
+          </Col>
+        </Row>
       </div>
     </div>
   );
