32f00d1c4b4c5448b59b6716dc88aa2a5703a688
Khanh Hung <hungtrandev97@gmail.com>
2020-11-25 17:51:17 +0700

    change

--- src/components/Admin/CategoryPage.js
+++ src/components/Admin/CategoryPage.js
@@ -23,60 +23,68 @@ import { getAllCategory } from "../../store/actions/actions";
 import "react-data-table-component-extensions/dist/index.css";
 import FormCreateCategory from "./FormCreateCategory";
 import FormEditCategory from "./FormEditCategory";
-import { confirmAlert } from "react-confirm-alert";
-import "react-confirm-alert/src/react-confirm-alert.css";
 import "./CategoryPage.css";
 
-const columns = memoizeOne((currentPage) => [
-  {
-    name: "S/N",
-    selector: "serial",
-    sortable: false,
-    center: true,
-    wrap: true,
-    width: "50px",
-    // format: (row) => (currentPage - 1) * PAGE_SIZE + (row.serial + 1),
-  },
-  {
-    name: "Tên Danh Mục",
-    selector: "category_title",
-    sortable: true,
-    center: true,
-    wrap: true,
-  },
-  {
-    name: "Thời Gian Tạo Danh Mục",
-    selector: "date_create",
-    sortable: false,
-    center: true,
-    wrap: true,
-    format: (row) => moment(row.date_create).format("DD-MM-DD hh:ss"),
-  },
-  {
-    name: "Sửa",
-    selector: "isDeleted",
-    sortable: false,
-    center: true,
-    wrap: true,
-    width: "80px",
-    format: (row) => (
-      <Link to={`/admin/editCategory/${row._id}`}>
-        <AiOutlineEdit size="1rem" color="rgb(250, 62, 63)" />
-      </Link>
-    ),
-  },
-  {
-    name: "xóa",
-    selector: "isDeleted",
-    sortable: false,
-    center: true,
-    wrap: true,
-    width: "80px",
-    cell: (row) => <RiDeleteBin6Line size="1rem" color="rgb(250, 62, 63)" />,
-  },
-]);
-
 function CategoryPage() {
+  const columns = memoizeOne((currentPage) => [
+    {
+      name: "S/N",
+      selector: "serial",
+      sortable: false,
+      center: true,
+      wrap: true,
+      width: "50px",
+      // format: (row) => (currentPage - 1) * PAGE_SIZE + (row.serial + 1),
+    },
+    {
+      name: "Tên Danh Mục",
+      selector: "category_title",
+      sortable: true,
+      center: true,
+      wrap: true,
+    },
+    {
+      name: "Thời Gian Tạo Danh Mục",
+      selector: "date_create",
+      sortable: false,
+      center: true,
+      wrap: true,
+      format: (row) => moment(row.date_create).format("DD-MM-DD hh:ss"),
+    },
+    {
+      name: "Sửa",
+      selector: "isDeleted",
+      sortable: false,
+      center: true,
+      wrap: true,
+      width: "80px",
+      format: (row) => (
+        <Link to={`/admin/editCategory/${row._id}`}>
+          <AiOutlineEdit size="1rem" color="rgb(250, 62, 63)" />
+        </Link>
+      ),
+    },
+    {
+      name: "xóa",
+      selector: "isDeleted",
+      sortable: false,
+      center: true,
+      wrap: true,
+      width: "80px",
+      cell: (row) => (
+        <RiDeleteBin6Line
+          onClick={() => submit(row._id)}
+          size="1rem"
+          color="rgb(250, 62, 63)"
+        />
+      ),
+    },
+  ]);
+
+  const submit = (id) => {
+    alert(id);
+  };
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getAllCategory());
