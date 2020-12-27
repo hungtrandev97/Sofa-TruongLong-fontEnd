3207ab8e849b0740901ffa997c1880b0b21dc5c7
Khanh Hung <hungtrandev97@gmail.com>
2020-12-27 19:10:00 +0700

    commit

--- src/ChatTest.js
+++ src/ChatTest.js
@@ -5,11 +5,11 @@ let socket;
 export default function ChatTest() {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-  const ENDPONT = "localhost:3002";
+  const ENDPONT = "localhost:4000";
   useEffect(() => {
     setName("hung");
     setRoom("chat");
-    socket = io("http://localhost:3002");
+    socket = io("http://localhost:4000");
     console.log(socket);
     socket.emit("join", { name, room });
   });
